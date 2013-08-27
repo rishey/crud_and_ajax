001e1f93f69376ebfe82b6a5a7b56c74149dba86
Kevin Solorio <soloriok@gmail.com>
2013-08-27 16:42:13 -0500

    Whoa!  way to much in one commit!  BAD KEVIN

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,58 @@
+# GET ================================================
+
 get '/' do
-  # Look in app/views/index.erb
+  @contacts = Contact.order('last_name').limit(10)
   erb :index
 end
+
+get '/contacts' do
+  @contacts = Contact.order('last_name')
+  erb :index
+end
+
+get '/contacts/new' do
+  erb :new, layout: !request.xhr?
+end
+
+get '/contacts/:id' do
+  @contact = Contact.find(params[:id])
+  erb :show
+end
+
+get '/contacts/:id/edit' do
+  @contact = Contact.find(params[:id])
+  erb :edit
+end
+
+
+
+# POST ===============================================
+
+post '/contacts' do
+  @contact = Contact.new(params[:contact])
+  if @contact.save
+    if request.xhr?
+      erb :_table_row, layout: false, locals: {contact: @contact}
+    else
+      redirect to("/contacts/#{@contact.id}")
+    end
+  else
+    erb :new
+  end
+end
+
+post '/contacts/:id' do
+  @contact = Contact.find(params[:id])
+  if @contact.update_attributes(params[:contact])
+    redirect to("/contacts/#{@contact.id}")
+  else
+    erb :edit
+  end
+end
+
+post '/contacts/:id/delete' do
+  @contact = Contact.find(params[:id])
+  @contact.destroy
+  redirect to("/contacts")
+end
+
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,30 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $('#new').click(function(event){
+    //Browser...Don't go to this link, I got it.
+    event.preventDefault();
+    $(this).hide();
+    var url = $(this).attr('href');
+
+    $.get(url, function(response){
+      $('.container').append(response);
+    });
+  });
+
+  $('.container').on('submit', '#new-form', function(event){
+    //Browser...Don't submit this...I've got it.
+    event.preventDefault();
+
+    var url = $(this).attr('action');
+    var data = $(this).serialize();
+
+    $.post(url, data, function(response){
+      //add a table row
+      $('tbody').append(response);
+    });
+    $(this).remove();
+    $('h1:last-child').remove();
+    $('#new').show();
+  });
+
 });
